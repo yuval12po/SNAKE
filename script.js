b2a670c7e408778a8a88bb702ff6151ff51d8336
yuval12po <yuvalarad333@gmail.com>
2026-06-29 12:28:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function getInput(){
+    let userInput = prompt("plese enter a number");
+    for(let i = 0; i<userInput;i++){
+        if (i % 2 ==0)
+            document.writeln(i);
+
+    }
+
+}
+
+
+
+getInput();
+
+function squared(){
+    let userInput = prompt("plese enter a number");
+    let squaedNum =userInput*userInput;
+    if(squaedNum % 2 ==0){   
+         document.writeln(squaedNum);
+
+
+    }
+
+}
+
+squared();
+ 
+function getInput(){
+    let userInput = prompt("plese enter a number");
+    let root = Math.sqrt(userInput);
+    document.writeln(root);
+
+    
+       
+
+
+
+
+}
