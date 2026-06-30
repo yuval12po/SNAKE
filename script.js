d21edd88ac079d6824522913930a40d91ef48695
yuval12po <yuvalarad333@gmail.com>
2026-06-30 11:29:14 +0300

    Update script.js
    Refactored the code to implement a Snake game, including functions for creating the game board, moving the snake, generating apples, and handling keyboard input.

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
-function getInput(){
-    let userInput = prompt("plese enter a number");
-    for(let i = 0; i<userInput;i++){
-        if (i % 2 ==0)
-            document.writeln(i);
-
+const grid = document.getElementById('grid');
+const scoreDisplay = document.getElementById('score');
+let squares = [];
+let currentSnake = [2, 1, 0];
+let direction = 1;
+let appleIndex = 0;
+let score = 0;
+let timerId = 0;
+let intervalTime = 200;
+function createBoard() {
+    for (let i = 0; i < 400; i++) {
+    const square = document.createElement('div');
+    grid.appendChild(square);
+    squares. push (square);
     }
-
 }
+createBoard();
 
-
-
-getInput();
-
-function squared(){
-    let userInput = prompt("plese enter a number");
-    let squaedNum =userInput*userInput;
-    if(squaedNum % 2 ==0){   
-         document.writeln(squaedNum);
-
-
-    }
-
+function startGame() {
+    //איפוס
+    currentSnake.forEach(index => squares[index].classList.remove('snake'));
+    squares[appleIndex].classList.remove('apple');
+    clearInterval(timerId);
+    currentSnake = [2, 1, 0];
 }
+function move() {
+    const hitbottom = (currentSnake[0] + 20 >= 400 && direction === 20);
+    const hitTop = (currentSnake[0] - 20 < 0 && direction === -20);
+    const hitright = (currentSnake[0] % 20 === 19 && direction === 1);
+    const hitleft = (currentSnake[0] % 20 === 0 && direction === -1);
+    const hitSelf = squares[currentSnake[0] + direction]?.classList.contains('snake');
+}
+function generateApple() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length);
+    } while (squares[appleIndex].classList.contains('snake'));
+    squares[appleIndex].classList.add('apple');
+}
+//פונקציה שמטפלת בשינוי כיוון לכפתורים ומקלדת
+function changeDir(newDirection) {
+   // מניעת פניית פרסה
+   if (direction + newDirection !== 0) {
+       direction = newDirection;
+   }
+}
+ //תמיכה במקלדת למחשב
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp') changeDir(-20);
+    if (e.key === 'ArrowDown') changeDir(20);
+    if (e.key === 'ArrowLeft') changeDir(-1);
+    if (e.key === 'ArrowRight') changeDir(1);
+});
 
-squared();
- 
-function getInput(){
-    let userInput = prompt("plese enter a number");
-    let root = Math.sqrt(userInput);
-    document.writeln(root);
+
+startGame();
+generateApple();
+
        
 
 
